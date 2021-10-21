c8e13c5b609c6411714659f16610e838abfff351
raitoku <raito_359279822@qq.com>
2021-10-21 15:56:44 +0800

    add sdk

--- src/test/java/com/huawei/demo/demo/DemoApplicationTests.java
+++ src/test/java/com/huawei/demo/demo/DemoApplicationTests.java
@@ -1,8 +1,16 @@
 package com.huawei.demo.demo;
 
 import com.huaweicloud.sdk.core.auth.BasicCredentials;
+import com.huaweicloud.sdk.core.auth.ICredential;
+import com.huaweicloud.sdk.core.exception.ConnectionException;
+import com.huaweicloud.sdk.core.exception.RequestTimeoutException;
+import com.huaweicloud.sdk.core.exception.ServiceResponseException;
 import com.huaweicloud.sdk.core.http.HttpConfig;
 import com.huaweicloud.sdk.core.invoker.SyncInvoker;
+import com.huaweicloud.sdk.rabbitmq.v2.RabbitMQClient;
+import com.huaweicloud.sdk.rabbitmq.v2.model.ListInstancesDetailsRequest;
+import com.huaweicloud.sdk.rabbitmq.v2.model.ListInstancesDetailsResponse;
+import com.huaweicloud.sdk.rabbitmq.v2.region.RabbitMQRegion;
 import com.huaweicloud.sdk.vpc.v3.VpcClient;
 import com.huaweicloud.sdk.vpc.v3.model.ListSecurityGroupsRequest;
 import com.huaweicloud.sdk.vpc.v3.model.ListSecurityGroupsResponse;
@@ -13,6 +21,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 @SpringBootTest
 class DemoApplicationTests {
+
+    /**
+     * user name
+     */
+    public final static String UN = "hid_v_4dks_c0qhvz8u";
+
+    /**
+     * project id
+     * 87408176acf348fbb17d7bfeacf62ccc
+     * 0df9c06d7800f2852f6dc00035a09c33
+     */
+    public final static String PROJECT_ID = "87408176acf348fbb17d7bfeacf62ccc";
+
+    /**
+     * Access Key Id
+     */
+    public final static String AK = "M5DZEMWZ9CBEF4WKLGVI";
+
+    /**
+     * Secret Access Key
+     */
+    public final static String SK = "5JjSxS24q4ExW1r5sdXWIahj8Z8jlTozUGw7S0tl";
 
     @Test
     void contextLoads() {
@@ -42,7 +72,34 @@ class DemoApplicationTests {
         SyncInvoker<ListSecurityGroupsRequest, ListSecurityGroupsResponse> responseSyncInvoker = vpcClient.listSecurityGroupsInvoker(new ListSecurityGroupsRequest());
         log.info("{}", responseSyncInvoker.toString());
 
+    }
 
+    @Test
+    public void testRabbitMqSdk() {
+
+        ICredential auth = new BasicCredentials()
+                .withProjectId(PROJECT_ID)
+                .withAk(AK)
+                .withSk(SK);
+
+        RabbitMQClient client = RabbitMQClient.newBuilder()
+                .withCredential(auth)
+                .withRegion(RabbitMQRegion.valueOf("cn-east-3"))
+                .build();
+        ListInstancesDetailsRequest request = new ListInstancesDetailsRequest();
+        try {
+            ListInstancesDetailsResponse response = client.listInstancesDetails(request);
+            System.out.println(response.toString());
+        } catch (ConnectionException e) {
+            e.printStackTrace();
+        } catch (RequestTimeoutException e) {
+            e.printStackTrace();
+        } catch (ServiceResponseException e) {
+            e.printStackTrace();
+            System.out.println(e.getHttpStatusCode());
+            System.out.println(e.getErrorCode());
+            System.out.println(e.getErrorMsg());
+        }
     }
 
 }
